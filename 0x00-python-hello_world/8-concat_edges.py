9fb5237228157d5e96654bc30845a6bd94d0d3f7
zakisu250 <zakisu250@gmail.com>
2023-06-05 06:15:01 -0700

    Concat string with slice

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+s = str[39:66]
+print(s + " with Python")
