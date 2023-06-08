643002b31e4c6fcc1d2e90e5977c2c1c056a1078
zakisu250 <zakisu250@gmail.com>
2023-06-08 06:15:20 -0700

    Imported calculator adjusted add

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-        args = sys.argv
+    args = sys.argv
     if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     a = int(args[1])
     b = int(argsif args[2] == '+':
     if args[2] == '+':
+        print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
     elif args[2] == '-':
         print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
     elif args[2] == '*':
