18e04a31b91db3651d1308846d20bf402e44d46d
zakisu250 <zakisu250@gmail.com>
2023-06-10 01:44:10 -0700

    Append and return new list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for n in my_list:
+        if (n % 2) == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return(newlist)
