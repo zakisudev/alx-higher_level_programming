9b08881c6526389e5f78c617457aa13330e8898f
zakisu250 <zakisu250@gmail.com>
2023-06-10 01:27:42 -0700

    fixed indentation

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list[:]
     else:
-        newlist = my_list[:]
-        newlist[idx] = element
-        return newlist
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
