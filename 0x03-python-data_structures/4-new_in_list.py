5f820b60cf617ee04dbd342cc9770481bb245540
zakisu250 <zakisu250@gmail.com>
2023-06-11 04:03:38 -0700

    header fic

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new = my_list.copy()
     if idx < 0 or idx > len(my_list):
-        return my_list[:]
+        return new
     else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+        new[idx] = element
+        return new
