094c74c264bbbe7d71e791405f48097a79bfc596
zakisu250 <zakisu250@gmail.com>
2023-06-14 11:22:31 -0700

    Complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new = a_dictionary.copy()
+    try:
+        for x in new:
+            if y == a_dictionary[x]:
+                del a_dictionary[x]
+        return (a_dictionary)
+    except KeyError:
+        return (a_dictionary)
