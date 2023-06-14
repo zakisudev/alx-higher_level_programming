76e1aa84ea402a847f2a8a38404a6ab3b5db7730
zakisu250 <zakisu250@gmail.com>
2023-06-14 11:08:33 -0700

    Added check for str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_to_int(roman_string):
+    if roman_to_int is None or type(roman_string) is not str:
         return (0)
     rom = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500,'M' : 100}
     con = 0
