4fb7fde34cb8e540f79df89b48a93d79471d526d
zakisu250 <zakisu250@gmail.com>
2023-06-14 08:48:58 -0700

    append fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,10 @@ def square_matrix_simple(matrix=[]):
         return [[]]
     if len(matrix[0]) == 0:
         return [[]]
-    new_matrix = [x ** 2 for row in matrix for x in row]
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for x in row:
+            new_row.append(x ** 2)
+        new_matrix.append(new_row)
     return new_matrix
