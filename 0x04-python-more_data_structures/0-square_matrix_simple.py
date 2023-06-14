b9cc41be19f4d2c8c0c62890cf7ea0634155183a
zakisu250 <zakisu250@gmail.com>
2023-06-14 08:04:45 -0700

    fixedcomparison error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return [[]]
-    if len(matrix[0] == 0):
+    if len(matrix[0]) == 0:
         return [[]]
     new_matrix = [x ** 2 for row in matrix for x in row]
     return new_matrix
