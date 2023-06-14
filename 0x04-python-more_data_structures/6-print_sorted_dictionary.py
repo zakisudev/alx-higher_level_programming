be92ae8ee3d0153eaf71c4eff592cf7f4251265d
zakisu250 <zakisu250@gmail.com>
2023-06-14 09:41:21 -0700

    fixed dictionary items()

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new = sorted(a_dictionary)
+    new = sorted(a_dictionary.items())
     for x, y in new:
         print("{}: {}".format(x, y))
