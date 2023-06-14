c435f5b762b9395790d1ca76136065be39380df6
zakisu250 <zakisu250@gmail.com>
2023-06-14 11:10:02 -0700

    Corrected for M value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_to_int is None or type(roman_string) is not str:
         return (0)
-    rom = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500,'M' : 100}
+    rom = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500,'M' : 1000}
     con = 0
     for x in range(len(roman_string)):
         if x > 0 and rom[roman_string[x]] > rom[roman_string[x - 1]]:
