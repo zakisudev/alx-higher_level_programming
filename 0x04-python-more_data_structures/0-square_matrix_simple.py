56577ebd1526697add292e272d907f2fbbce71da
zakisu250 <zakisu250@gmail.com>
2023-06-14 07:28:02 -0700

    fixed other options

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if len(matrix) == 0:
+        return [[]]
+    if len(matrix[0] == 0):
+        return [[]]
     new_matrix = [x**2 for elem in matrix for x in elem]
     return new_matrix
