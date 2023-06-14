1b207341824b156c7eca3acdfdd086383d52b55f
zakisu250 <zakisu250@gmail.com>
2023-06-14 08:59:47 -0700

    colon added to if statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for x in my_list:
-        if x == search
+        if x == search:
             new_list.append(replace)
         else:
             new_list.append(x)
