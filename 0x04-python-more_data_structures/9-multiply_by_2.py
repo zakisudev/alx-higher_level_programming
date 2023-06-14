d8fc273b75dad2462c34df02887aef83b95a7ba8
zakisu250 <zakisu250@gmail.com>
2023-06-14 09:55:08 -0700

    Updated value print

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for x, y in a_dictionary:
+    for x, y in a_dictionary.items():
         new = {}
-        new.append(x, y ** 2)
+        new[y] = y ** 2
     return new
