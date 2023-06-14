2d4111ea178e2ec207f03e0cf2972ee10bf4dfa6
zakisu250 <zakisu250@gmail.com>
2023-06-14 10:10:24 -0700

    removed for loopwq

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for x, y in a_dictionary:
-        if len(a_dictionary) == 0 or a_dictionary is None:
-            return None
-        else:
-            x = list(a_dictionary.keys())
-            y = list(a_dictionary.values())
-            return (x[y.index(max(y))])
+    if len(a_dictionary) == 0:
+        return None
+    else:
+        x = list(a_dictionary.keys())
+        y = list(a_dictionary.values())
+        return (x[y.index(max(y))])
