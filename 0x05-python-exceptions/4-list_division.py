f6512e24a982c336837be43fbb3fe05b9a6b9b93
zakisu250 <zakisu250@gmail.com>
2023-06-26 09:29:29 -0700

    spelling error for list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     ans = 0
     for i in range(list_length):
         try:
-            ans = my_list[i] / my_list_2[i]
+            ans = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             ans = 0
             print("division by 0")
