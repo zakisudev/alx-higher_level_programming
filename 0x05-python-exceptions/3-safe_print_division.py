aede4e2ee899195b6995ddb1deca914f30c46cea
zakisu250 <zakisu250@gmail.com>
2023-06-26 09:17:26 -0700

    spelling error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return
     finally:
-        print("Inside result: {:.1}".format(res))
+        print("Inside result: {}".format(res))
     return (res)
