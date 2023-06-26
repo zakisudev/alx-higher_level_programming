2bde971bfc5e55f3362aa6b21cc400e71acea978
zakisu250 <zakisu250@gmail.com>
2023-06-26 09:26:40 -0700

    result spelling error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,14 +7,14 @@ def list_division(my_list_1, my_list_2, list_length):
             ans = my_list[i] / my_list_2[i]
         except ZeroDivisionError:
             ans = 0
-            print("Division by 0")
+            print("division by 0")
             continue
         except TypeError:
-            print("Wrong Type")
+            print("wrong type")
             continue
         except IndexError:
             ans = 0
-            print("Out of Range")
+            print("out of range")
             continue
         finally:
             newList.append(ans)
