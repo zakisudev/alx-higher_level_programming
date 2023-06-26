ad62c66686067b616cf4b0bb631d62d9d7be2d3c
zakisu250 <zakisu250@gmail.com>
2023-06-26 08:30:05 -0700

    adjusted the exception answer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    answer = false
     try:
-        value = int(value)
         print("{:d}".format(value))
-        return (True)
-    except ValueError:
-        return (False)
+        answer = True
+    except:
+        answer = False
+    return (answer)
