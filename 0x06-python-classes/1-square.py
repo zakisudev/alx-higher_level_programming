daf753b5e47b5290ef95a3b3d48e4487fc449170
zakisu250 <zakisu250@gmail.com>
2023-06-28 00:23:00 -0700

    Initialize a square from a class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+" Represent a square "
+
+    def __init__(self, size):
+        """ Initializes a new Square
+
+        Args:
+            size (int): The size of the new Square
+        """
+        self.__size = size
