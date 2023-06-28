769cd87a4fd767876e9801ee1d41bdc9ff9a908a
zakisu250 <zakisu250@gmail.com>
2023-06-28 01:38:03 -0700

    spelling error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,19 +4,19 @@
 
 
 class Square:
-    " Represent a square instance "
+    "Represent a square."
 
     def __init__(self, size=0):
-        """ Initialize the new square
+        """Initialize a new square.
         Args:
-            size (int): The size of the new square
-            """
-        if not isintance(size, int):
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        " Calculates and returns the area of the new square "
+        "Return the current area of the square."
         return (self.__size * self.__size)
