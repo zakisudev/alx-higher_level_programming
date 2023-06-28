cdb6d710fb40c7e320a86b4cb53ab5865057bef3
zakisu250 <zakisu250@gmail.com>
2023-06-28 01:18:47 -0700

    corrected elif statement

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,6 +14,6 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
-        else size < 0:
+        elif size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
