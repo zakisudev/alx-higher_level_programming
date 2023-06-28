21b5afe7236b95872c7c4e995f6aa6a2ab976656
zakisu250 <zakisu250@gmail.com>
2023-06-28 00:25:51 -0700

    fixed pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+" Define a class Square "
+
+
 class Square:
-" Represent a square "
+    " Represent a square "
 
     def __init__(self, size):
         """ Initializes a new Square
