28531e2ddc3f471c135d386de0113d1ba0688d30
zakisu250 <zakisu250@gmail.com>
2023-07-03 06:21:30 -0700

    Calculating Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Defines a new class Rectangle """
+
+
+class Rectangle():
+    """ Initializes a new rectanlge """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """ Getter for height value """
+        return (self.__height)
+
+    @property
+    def width(self):
+        """ Getter for width value """
+        return (self.__width)
+
+    @height.setter
+    def height(self, value):
+        """ Checkes and set the height to value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """ Checks and sets the width value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """ Calculates and returns the area of the new rectangle """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """ Calculates and returns the perimeter of the new rectangle """
+        return (self.__width * 2 + self.__height * 2)
