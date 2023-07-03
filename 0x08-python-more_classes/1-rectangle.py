8896a98bb0d84cbf11fd8f379fd95011d134cbc5
zakisu250 <zakisu250@gmail.com>
2023-07-03 06:06:08 -0700

    Comments added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,14 +9,17 @@ class Rectangle():
         self.width = width
         self.height = height
 
+    """ Gets the height value """
     @property
     def height(self):
         return (self.__height)
 
+    """ Gets the width value """
     @property
     def width(self):
         return (self.__width)
-    
+
+    """ Checks and sets the height value """
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -25,6 +28,7 @@ class Rectangle():
             raise ValueError("width must be >= 0")
         self.__height = value
 
+    """ Checks and sets the width value """
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
