ea28895048fad05b00a2a2c7f3d4549b47472d99
zakisu250 <zakisu250@gmail.com>
2023-07-03 06:09:20 -0700

    Privatized the object instances

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,17 +11,17 @@ class Rectangle():
 
     """ Gets the height value """
     @property
-    def height(self):
+    def __height(self):
         return (self.__height)
 
     """ Gets the width value """
     @property
-    def width(self):
+    def __width(self):
         return (self.__width)
 
     """ Checks and sets the height value """
     @height.setter
-    def height(self, value):
+    def __height(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -30,7 +30,7 @@ class Rectangle():
 
     """ Checks and sets the width value """
     @width.setter
-    def width(self, value):
+    def __width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
