734cad40ed12d0fbf01208ddedf584395033fd58
zakisu250 <zakisu250@gmail.com>
2023-07-04 04:23:38 -0700

    correction

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string(last=[]):
+    last.append("BestSchool")
+    return ", ".join(last)
