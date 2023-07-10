7622ed6ecb920690e389714bcdf0839336fdca20
zakisu250 <zakisu250@gmail.com>
2023-07-10 07:53:12 -0700

    Print a string format

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Import the Basegeometry module """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+""" A derived class that inherits from Basegeometry class """
+
+
+class Square(Rectangle):
+
+    """ inistantiates with size """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Sqaure] {}/{}".format(self.__size, self.__size)
