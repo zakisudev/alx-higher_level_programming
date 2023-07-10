2d1268a7503d6a9bb383f6cc34bb7dd64cec6deb
zakisu250 <zakisu250@gmail.com>
2023-07-10 05:30:26 -0700

    Raising an error for instance methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Defines an empty class BaseGeometry  """
+
+
+class BaseGeometry:
+    """ Defines an instance method area """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """ public instance method integer_validator """
+    def integer_validator(self, name, value):
+
+        self.name = name
+        """ Raise a typeerror if value is not an integer """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        """ Raise a valueerror if value is less or equal to 0 """
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
