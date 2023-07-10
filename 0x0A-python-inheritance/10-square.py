1b1109a1883f3d3a3541f30288afb97089267794
zakisu250 <zakisu250@gmail.com>
2023-07-10 07:47:14 -0700

    removed area

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
 """ Import the Basegeometry module """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle= __import__('9-rectangle').Rectangle
+
+""" A derived class that inherits from Basegeometry class """
 
 
-""" A base class that inherits from Basegeometry class """
-
-
-class Rectangle(BaseGeometry):
-
-    """ instaniates width and height and
-    validates using Parent class method """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    """ area method implementation """
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 class Square(Rectangle):
 
     """ inistantiates with size """
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(self, size)
         self.__size = size
-
-    """ Implements the area calculation of a square with (size*size) """
-    def area(self):
-        return self.__size * self.__size
