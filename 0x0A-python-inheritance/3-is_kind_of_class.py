76442595c5cdf141cf7e28df6dcd68765e3d2117
zakisu250 <zakisu250@gmail.com>
2023-07-10 04:54:15 -0700

    Check if object is instance of class or instance of inhereted class of specifed class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines is_kid_of_class function  """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns if an object is an instance of class
+    or inhereted class instance of a specified class """
+
+    return isinstance(obj, a_class)
