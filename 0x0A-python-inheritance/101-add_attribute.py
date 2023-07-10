1e470d1ec606c80ca23222423fd884b77cab7757
zakisu250 <zakisu250@gmail.com>
2023-07-10 08:09:34 -0700

    Add attribute if possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+""" Defines a function to add attributes if possible """
+
+
+def add_attribute(obj, att, value):
+
+    """ checks if not possible """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attributes")
+
+    """ sets the attribute value """
+    setattr(obj, attribute, value)
