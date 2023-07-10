a6ca9328f56bd5b40085b5046ba4130fd2e1fb31
zakisu250 <zakisu250@gmail.com>
2023-07-10 08:42:43 -0700

    pycodestyle erro fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
-""" Defines a function to add attributes if possible """
+#!/usr/bin/python3
+""" a functio add_attribute to add attribute """
 
 
 def add_attribute(obj, attribute, value):
+    """ Accepts three parameters (obj, attribute, value).
+    Adds a new attribute to an object if it’s possible.
+    """
 
-    """ checks if not possible """
     if not hasattr(obj, "__dict__") and not isinstance(obj, type):
-       raise TypeError("can't add new attribute")
-
-    """ sets the attribute value """
+        raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
