503ef889c4dc9704c19b2e57146252e022efc50f
zakisu250 <zakisu250@gmail.com>
2023-07-10 05:01:13 -0700

    Added for not an object

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """ Returns if an object is an instance of inhereted
     class of specified class """
 
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
