fc4d36061db40d836313cb96cba78e551a84a2b0
zakisu250 <zakisu250@gmail.com>
2023-07-10 05:14:50 -0700

    Create an exception error message for an instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines an empty class BaseGeometry  """
+
+
+class BaseGeometry:
+    """ Defines an instance method area """
+
+    def area(self):
+        raise Exception("area() is not implemented")
