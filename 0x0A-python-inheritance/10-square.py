4d657d8790d41c67e91c8c40786152a1342198ea
zakisu250 <zakisu250@gmail.com>
2023-07-10 07:48:58 -0700

    misspell

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,5 +11,5 @@ class Square(Rectangle):
     """ inistantiates with size """
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__(self, size)
+        super().__init__(size, size)
         self.__size = size
