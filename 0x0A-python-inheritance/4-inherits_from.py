c1d29d1c269972db796e5e35b4d4bf001bb3bb07
zakisu250 <zakisu250@gmail.com>
2023-07-10 04:59:37 -0700

    Check if an object is a subclass of an instance of a specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines inherets_from function  """
+
+
+def inherits_from(obj, a_class):
+    """ Returns if an object is an instance of inhereted
+    class of specified class """
+
+    return issubclass(type(obj), a_class)
