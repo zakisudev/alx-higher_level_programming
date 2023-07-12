608651d91a673f2e9c6ee0e25393df960739c981
zakisu250 <zakisu250@gmail.com>
2023-07-12 04:17:04 -0700

    spelling error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ def save_to_json_file(my_obj, filename):
     """
     A function that writes to a text file using json representation
     """
-    with open("filename", mode"r", encoding="UTF-8") as f:
+    with open("filename", mode="w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
