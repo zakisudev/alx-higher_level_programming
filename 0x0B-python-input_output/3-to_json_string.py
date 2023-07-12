c3ba398787c72ed3563e7572aee89f289d41b187
zakisu250 <zakisu250@gmail.com>
2023-07-12 04:01:52 -0700

    Outputs a json representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" import json module"""
+import json
+
+
+""" A function that returns a json representation of an object """
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
