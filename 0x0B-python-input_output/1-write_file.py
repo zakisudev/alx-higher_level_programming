89d663197002bad68dd4d0a4aa118ef89e758855
zakisu250 <zakisu250@gmail.com>
2023-07-12 03:14:26 -0700

    Write a text to a file and return the character bytes

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A function to write a text file and returns tje chars written """
+
+
+def write_file(filename="", text=""):
+    """ takes an input filename and writes a text file
+    and returns the number of chars written """
+
+    with open(filename, mode='w', encoding="UTF-8") as f:
+        f.write(text)
+        f.tell()
