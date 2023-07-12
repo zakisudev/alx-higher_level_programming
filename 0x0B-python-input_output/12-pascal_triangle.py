0c55e66a412b49b09d17271339c291c2fb92967e
zakisu250 <zakisu250@gmail.com>
2023-07-12 05:48:51 -0700

    Pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Define a pascal triangle function """
+
+
+def pascal_triangle(n):
+    """ defines a pascal triangle of size n """
+    if n<= 0:
+        return []
+
+    tris = [[1]]
+    while len(tris) != n:
+        tr = tris[-1]
+        temp = [1]
+        for i in range(len(tr) - 1):
+            temp.append(tr[i] + tr[i + 1])
+        temp.append(1)
+        tris.append(temp)
+    return tris
