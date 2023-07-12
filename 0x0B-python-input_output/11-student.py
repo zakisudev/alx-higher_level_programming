bfdb680db7f2d117f2b6c22da57ade0c22c1061f
zakisu250 <zakisu250@gmail.com>
2023-07-12 05:38:29 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Parent class Student """
+
+
+class Student:
+
+    """ initializes Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        """ Check and represent in json """
+        if(type(attrs) == list and all(type(e) == str for e in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+
+        """ replace all attrs of the Student """
+        for x, y in json.items():
+            setaatr(seff, x, y)
