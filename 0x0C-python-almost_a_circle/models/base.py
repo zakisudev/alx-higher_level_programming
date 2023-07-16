dc4e136c2b999dfb51645d82aa1b7a504872dd6a
zakisu250 <zakisu250@gmail.com>
2023-07-16 08:03:54 -0700

    Base class implementation

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  Base Class """
+
+
+class Base:
+    __nb_objects = 0
+    """  Initializes the class """
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
