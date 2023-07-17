fcf2176e8e79efd26ad9a8a87f68dec042c29d24
zakisu250 <zakisu250@gmail.com>
2023-07-17 00:55:22 -0700

    pycodestyle fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base:
     __nb_objects = 0
     """  Initializes the class """
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
