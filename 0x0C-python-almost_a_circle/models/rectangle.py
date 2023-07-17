df0cd464a85627b5cf40584faf39887eedbc94c8
zakisu250 <zakisu250@gmail.com>
2023-07-17 07:53:40 -0700

    Rectangle subclass that inherits from Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,9 +35,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     """ return decorator for x """
@@ -50,7 +50,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     """ Decorator for y """
@@ -63,5 +63,58 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    """ Area calculator """
+    def area(self):
+        return self.width * self.height
+
+    """ Displaying # """
+    def display(self):
+        for i in range(self.y):
+            print()
+
+        for j in range(self.height):
+            for k in range(self.x):
+                print(" ", end="")
+            for h in range(self.width):
+                print("#", end="")
+
+    """ String presentation of the result """
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.x, self.y, self.width, self.height))
+
+    """ Assigning arguments to each attribute """
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[4]
+        else:
+            for ky, vl in kwargs.items():
+                if ky == 'id':
+                    self.id = vl
+                elif ky == 'width':
+                    self.width = vl
+                elif ky == 'height':
+                    self.height = vl
+                elif ky == 'x':
+                    self.x = vl
+                elif ky == 'y':
+                    self.y = vl
+
+    """ Dictionary representation of the Rectangle class """
+    def to_dictionary(self):
+        dict = {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
+        return dict
