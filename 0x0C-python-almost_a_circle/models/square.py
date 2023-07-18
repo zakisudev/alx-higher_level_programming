6f88039df198ee618325ca284acd17f5f64cd5d7
zakisu250 <zakisu250@gmail.com>
2023-07-18 04:26:30 -0700

    added condition for size value

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,6 +38,10 @@ class Square(Rectangle):
         Args:
             value(int): the value to assign to size
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
         self.__size = value
