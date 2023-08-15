7a89c1c6181143ccd6bbade55bafbdbbf12ca7b3
zakisu250 <zakisu250@gmail.com>
2023-08-15 12:47:53 -0700

    added order by parameter

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Import hbtn_0c_0 and display top 3
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 ORDER BY city GROUP BY avg_temp DESC LIMIT 3;
