d94b6fac8a41ae7a1805ae6f2b084aa3cf2b8f03
Zekaria Mohammed <36282172+zakisu250@users.noreply.github.com>
2023-08-16 22:06:53 +0300

    Writing method changed

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,5 @@
 -- Import hbtn_0c_0 and display top 3
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 ORDER BY city GROUP BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp 
+FROm temperatures
+WHERE month=7 OR month=8 
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
