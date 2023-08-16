adf309bdd74e0831a12a8ddff5b3aa8a1a070d90
zakisu250 <zakisu250@gmail.com>
2023-08-16 11:47:15 -0700

    List all genres in their descending rating values

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- List all genres in their raiting
+
+SELECT tv_genres.name, SUM(rate) AS rating
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
