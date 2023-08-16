caf318e7c9b76d954f8d587da2edd1eff32c8d51
zakisu250 <zakisu250@gmail.com>
2023-08-16 11:08:55 -0700

    List all content of shows from database with joining

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows in a database
+
+SELECT tv_shows.title, tv_show_genres.genre.-r
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
