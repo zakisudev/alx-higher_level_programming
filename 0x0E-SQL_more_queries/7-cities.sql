d383605a85e5f52c0567d98e1af05d37ca60f80a
zakisu250 <zakisu250@gmail.com>
2023-08-16 10:57:57 -0700

    Create hbtn database and cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create database and table with nonNull city name and id
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id));
