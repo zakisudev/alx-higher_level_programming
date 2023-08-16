6d1adcd616ab2d60c84b1f91890431c52d47860f
zakisu250 <zakisu250@gmail.com>
2023-08-16 09:32:26 -0700

    indentation fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- Create mysql server user user_0d_1D
-CREATE USER IF NOT EXISTS
-	'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.* TO 'user_0d_1'@'localhost';
+    ON *.*
+    TO 'user_0d_1'@'localhost';
