9c471e6aececde01d612b0f7099c5cc19ad876b1
zakisu250 <zakisu250@gmail.com>
2023-08-16 11:51:12 -0700

    spelling fixes

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,5 +6,5 @@ WHERE tv_shows.id NOT IN(
 SELECT tv_show_genres.show_id
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_genres,name='Comedy')
+WHERE tv_genres.name='Comedy')
 ORDER BY tv_shows_title ASC;
