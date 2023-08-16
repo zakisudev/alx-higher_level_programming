55628cea9e1f381524c7a6421f4effac436fa2aa
zakisu250 <zakisu250@gmail.com>
2023-08-16 11:11:24 -0700

    List all shows without genre link

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- List all shows without genre link
+
+SELECT tv_shows.title, tv_show_genres.genres_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres,genre_id ASC;
