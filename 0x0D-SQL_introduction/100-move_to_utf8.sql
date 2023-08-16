e55de160b807f13c08e97058c6d2237cec9a7e4c
Zekaria Mohammed <36282172+zakisu250@users.noreply.github.com>
2023-08-16 22:07:57 +0300

    Changed methodology for Granting

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- Change database to UTF-8
 USE hbtn_0c_0;
 -- Change name to UTF-8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
