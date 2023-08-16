d59ea7575d897ec89ad56ee27ecd97351f5d82b3
zakisu250 <zakisu250@gmail.com>
2023-08-16 02:03:34 -0700

    file spelling error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Change database to UTF-8
-USE htbn_0c_0;
+USE hbtn_0c_0;
 -- Change name to UTF-8
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
