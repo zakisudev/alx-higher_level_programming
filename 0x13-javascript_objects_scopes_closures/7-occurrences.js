d9831a3e76451d8bdd22b6fc7caecb58c10221fc
zakisu250 <zakisu250@gmail.com>
2023-09-11 09:07:02 -0700

    Reverse Log Convert and occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; i <list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin.node
 exports.esrever = function (list) {
-  const reverse = [];
-  for (let i= list.length - 1; i >= 0; i--) {
-    reverse.push(list[i]);
+  const reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
   }
-  return reverse;
-}
+  return reversed;
+};
