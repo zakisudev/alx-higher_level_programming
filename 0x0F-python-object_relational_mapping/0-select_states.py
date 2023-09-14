fe6abfe3feda49145aa2258e331ed137efcb45a7
zakisu250 <zakisu250@gmail.com>
2023-09-14 06:35:49 -0700

    fetch all states from the database and sort them

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" list all states from the database 'hbtn_0e_0_usa' """
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    """ connect to database """
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    """ create cursor """)
+    cur = db.cursor()
+    cu.execute('SELECT*
+                FROM states
+                ORDER BY id DESC')
+    for rec in cur.fetchall():
+        print(rec)
+    cur.close()
+    db.close
