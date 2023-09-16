101f9727cd8ba3f1aad9a4e6f3979588498bba54
zakisu250 <zakisu250@gmail.com>
2023-09-16 02:13:12 -0700

    Delete states from the db by their name

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Add 'Louisiana' to the state in databse """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    new_session = session()
+
+    states = new_session.query(State).filter(State.name.like('%a%'))
+    for state in states.all():
+        new_session.delete(state)
+
+    new_session.commit()
+    new_session.close()
