4577df0aaf605de204e1900dc5fb4fc305f705cd
zakisu250 <zakisu250@gmail.com>
2023-09-16 02:24:14 -0700

    City model class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines City class """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ implements the city class that inherits from Base """
+    __table_name = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer)
