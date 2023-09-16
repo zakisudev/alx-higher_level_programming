4dad66d3c1e6962ce348c886344b5e1a60a1d939
zakisu250 <zakisu250@gmail.com>
2023-09-16 01:37:39 -0700

    creating a Stat class model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a State class """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Assigned to extracted states in the table """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
