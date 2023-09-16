4080a18b6ec077535b12b77c8b363de208cee674
zakisu250 <zakisu250@gmail.com>
2023-09-16 02:30:07 -0700

    Improved connection between state and city classes

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" List all state objects by using sqlalchemy """
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm.session import sessionmaker, Session
+from sqlalchemy import create_engine
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(arg[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    new_session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Fransisco', state=new_state)
+    new_state.cities.append(new_city)
+    new_session.add(new_state)
+    new_session.add(new_city)
+    session.commit()
