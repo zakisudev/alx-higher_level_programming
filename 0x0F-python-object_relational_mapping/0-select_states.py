9fa0bda8ba1fc7c2208799bf3bd178f28981f1df
zakisu250 <zakisu250@gmail.com>
2023-09-16 00:39:59 -0700

    executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """ list all states from the database 'hbtn_0e_0_usa' """
+from sys impoty argv
+import MySQLdb
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     """ connect to database """
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=argv[1],
                          password=argv[2],
                          database=argv[3])
-    """ create cursor """
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
-    """ display the record """
+
     for rec in cur.fetchall():
         print(rec)
-    cur.close()
-    db.close
