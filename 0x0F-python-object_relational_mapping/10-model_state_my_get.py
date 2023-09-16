5b74c4d9353892127bf701dae4f09b45b919285c
zakisu250 <zakisu250@gmail.com>
2023-09-16 10:58:07 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -3,17 +3,17 @@
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
-    states = session().query(State).filter(state.name == argv[4])
+    states = session().query(State).filter(State.name == argv[4])
     if (states.count() == 0):
         print("Not found")
     else:
         for state in states:
-        print("{}: {}".format(state.id, ))
+            print("{}: {}".format(state.id, state.name))
     session().close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,14 +4,14 @@ from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
-    new_session = session()
+    Session = sessionmaker(bind=engine)
+    new_session = Session()
 
     data = new_session.query(State, City)\
             .join(State, State.id == City.state_id).order_by(City.id)
