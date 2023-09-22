0c11d4e0393f7b577ea0f6c3922768392e22a998
zakisu250 <zakisu250@gmail.com>
2023-09-21 23:12:03 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -9,6 +9,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    """ create engine, add city and commit """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
 
