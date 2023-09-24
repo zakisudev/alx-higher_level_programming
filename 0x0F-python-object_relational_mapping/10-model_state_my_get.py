6d08d26d9411279c80e2b2614d7496a1567a02ea
zakisu250 <zakisu250@gmail.com>
2023-09-23 23:54:35 -0700

    error for Base metadata fixed

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,12 +8,11 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
-    states = session().query(State).filter(State.name == argv[4])
+    states = session().query(State).filter(State.name == argv[4]).first()
+
     if (states.count() == 0):
         print("Not found")
     else:
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
+        print(states.id)
     session().close()
