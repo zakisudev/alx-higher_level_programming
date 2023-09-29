205b687fd8f4db0e52ff7eebbd4a3f4c84662dd4
zakisu250 <zakisu250@gmail.com>
2023-09-29 16:48:21 +0300

    send a request and display the error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" send a request to url and display the response decoded with ut-8 """
+from urllib.request import Request, urlopen
+from urllib,error import HTTPError
+import sys
+
+if __name__ == "__main__":
+    request = Request(argv[1])
+    try:
+        with urlopen(request) as response:
+            res = response.read()
+            print(res.decode('utf-8'))
+    except HTTPError as err:
+        print('Error code:', err.code)
