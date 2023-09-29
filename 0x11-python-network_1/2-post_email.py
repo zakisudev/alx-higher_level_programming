8df1fa2dcc0ed35b60c3a6459e1f8666c2104079
zakisu250 <zakisu250@gmail.com>
2023-09-29 16:51:48 +0300

    corrected sys import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ send a POST request with url & email and displays the response """
 from urllib import request, parse
-import argv
+import sys
 
-if len(argv) > 2:
-    email = {'email': argv[2]}
+if len(sys.argv) > 2:
+    email = {'email': sys.argv[2]}
     data = parse.urlencode(email).encode('ascii')
-    req = request.Request(argv[1], data)
+    req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
         res = response.read()
         print(res.decode('utf-8'))
