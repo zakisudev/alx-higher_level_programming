69b2676d49ae1dfb9ff03964a52c48e0e318b020
zakisu250 <zakisu250@gmail.com>
2023-09-29 16:19:33 +0300

    request to holberton url and display the X-Request-Id value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" request a url and display the X-Request-Id variable in the response """
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.getheader('X-Request-Id')
+    print(x_request_id)
