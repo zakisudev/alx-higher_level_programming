cb591db904a87ac0866d0ac7cba53c0c67251af3
zakisu250 <zakisu250@gmail.com>
2023-09-29 16:37:26 +0300

    added variables

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,13 @@
 """ Fetching a url to print response """
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    response_body = response.read()
-    utf8_content = response_body.decode('utf-8')
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
 
-print("Body response:")
-print(f"    - type: {type(response_body)}")
-print(f"    - content: {response_body}")
-print(f"    - utfcontent: {utf8_content}")
+        print("Body response:")
+        print(f"\t- type: {type(response_body)}")
+        print(f"\t- content: {response_body}")
+        print(f"\t- utfcontent: {utf8_content}")
