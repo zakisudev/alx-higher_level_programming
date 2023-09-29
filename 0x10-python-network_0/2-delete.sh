93b70cf0ad91d541429086dc600f4038e96ea8bd
zakisu250 <zakisu250@gmail.com>
2023-09-29 15:13:42 +0300

    updated script

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # Sending DELETE request to a server and displaying the response
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-URL="$1"
-res=$(mktemp)
-curl -X DELETE -s -o "$res" "$URL"
-cat "$res"
-rm "$res"
+curl -sX "DELETE" "$1"
