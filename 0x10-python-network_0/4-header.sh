788200f5cf213d1cb5faaeeff739b95fe4d0f4e4
zakisu250 <zakisu250@gmail.com>
2023-09-29 15:16:29 +0300

    simplified the script

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # send a GET request with an id attribute
 
-if [ $# -eq 0]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-URL="$1"
-curl -H "X-School-User-Id: 90" -s "$URL"
+curl -sH "X-School-User-Id: 98" "$1"
