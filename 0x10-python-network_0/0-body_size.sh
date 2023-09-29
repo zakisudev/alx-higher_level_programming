d0255b9684a4ffd33f749aa4541619d1c5f5fd36
zakisu250 <zakisu250@gmail.com>
2023-09-29 15:12:59 +0300

    spelling correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 # request and display the size of the body of the response
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-res=$(mktemp)
-curl -s -o "$res" "$1"
-res_size=$(stat -c%s "$res")
-
-echo "$res_size"
-rm "$res"
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 # send a GET request to a url and display the body of the response
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-URL="$1"
-res=$(mktemp)
-curl -s -o "$res" "$URL"
-
-http=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-if [ "$http" -eq 200 ]; then
-	cat "$res"
-fi
-rm "$res"
+curl -sL "$1"
