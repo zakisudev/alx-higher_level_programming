a1cc4af9d1354c8b18dd2f4334017378f4ffa0b9
zakisu250 <zakisu250@gmail.com>
2023-09-29 15:17:46 +0300

    refactores script

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # send a POST request with email and subject attributes
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit
-fi
-URL="$1"
 EMAIL="test@gmail.com"
 SUBJECT="I will always be here for PLD"
-curl -X POST -d "email=$EMAIL&subject=$SUBJECT" -s "$URL"
+curl -sX POST -d "email=$EMAIL&subject=$SUBJECT" "$1"
