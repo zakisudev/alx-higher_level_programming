a72c399fb272927e9535db3defb07f1efc86c8f6
zakisu250 <zakisu250@gmail.com>
2023-09-29 15:15:29 +0300

    changed the display method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Identify the HTTP requests a url is accepting
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-URL="$1"
-curl -X OPTIONS -I "$URL" | grep "Allow:" | sed 's/Allow: //'
+curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
