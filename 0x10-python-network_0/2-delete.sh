1fd46323427ada92961c01ea865ca787d55fd10f
zakisu250 <zakisu250@gmail.com>
2023-09-30 10:42:24 +0300

    three line code correction

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sending DELETE request to a server and displaying the response
+# Sending DELETE request to server and displaying the response
 curl -sX "DELETE" "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Identify the HTTP requests a url is accepting
-
 curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
