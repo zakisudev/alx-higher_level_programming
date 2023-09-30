db63fc423ac8af709560122ca7c3550c3a858284
zakisu250 <zakisu250@gmail.com>
2023-09-30 11:15:36 +0300

    execution mode added

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # send a POST request with a JSON data
-
 curl -X POST -d "@$2" -H "Content-Type: application/json" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # send a request to a url and display custome text
-
-url="0.0.0.0:5000/catch_me"
-curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "$url"
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
