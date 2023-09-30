b2433b09d6c8bd41da1fc97de4e603d817ae4cef
zakisu250 <zakisu250@gmail.com>
2023-09-30 11:53:00 +0300

    corrected the function

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,10 @@
 from urllib import request, parse
 import sys
 
-if __name == "__main__" :
+if len(sys.argv) > 2:
     email = {'email': sys.argv[2]}
     data = parse.urlencode(email).encode('ascii')
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
-        res = response.read()
-        print(res.decode('utf-8'))
+        r = response.read()
+        print(r.decode('utf-8'))
