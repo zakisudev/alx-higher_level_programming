2477341fccd014632c858b3056527cb1e628c5ec
zakisu250 <zakisu250@gmail.com>
2023-09-30 11:32:15 +0300

    removed running when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,11 @@
 """ Fetching a url to print response """
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
-
-        print("Body response:")
-        print(f"\t- type: {type(response_body)}")
-        print(f"\t- content: {response_body}")
-        print(f"\t- utfcontent: {utf8_content}")
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utfcontent:", utf_content)
