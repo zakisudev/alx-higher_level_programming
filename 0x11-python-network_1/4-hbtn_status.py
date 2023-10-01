cbdcd5eca0b81ee9edd50a3a13f7bf785338f9e2
zakisu250 <zakisu250@gmail.com>
2023-10-01 15:18:53 +0300

    corrected code to print three lines

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ send request and display the X-Request-Id value """
 import requests
-import sys
 
 if __name__ == "__main__":
-    res = requests.get(sys.rgv[1])
-    print(res.headers.get('X-Request-Id'))
+    res = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('    - type:', type(res.text))
+    print('    - content:', res.text)
