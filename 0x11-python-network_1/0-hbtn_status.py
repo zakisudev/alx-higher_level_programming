35102103095bbed94aa27332c7926e0cdf25e594
zakisu250 <zakisu250@gmail.com>
2023-10-01 11:03:21 -0700

    removed expected new line from the decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,9 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         html = res.read()
+        utf = html.decode('utf-8')
+        utf8 = utf.replace('\r', '')
         print('Body response:')
-        print('    - type: {}'.format(type(html)))
-        print('    - content: {}'.format(html))
-        print('    - utf8 content: {}'.format(html.decode('utf-8')))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(utf8))
