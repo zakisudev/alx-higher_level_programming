5912bd54bb799567603f72978515d1433e60036c
zakisu250 <zakisu250@gmail.com>
2023-10-01 11:09:08 -0700

    pycodestyle fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('    - type:', type(res.text))
-    print('    - content:', res.text)
+    print('\t- type:', type(res.text))
+    print('\t- content:', res.text)
