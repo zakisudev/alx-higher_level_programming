39fb4a197900e59f0714fa00ff798eb7c11816c5
zakisu250 <zakisu250@gmail.com>
2023-10-24 10:22:47 +0300

    semistandard check

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs')
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data)
+    process.stdout.write(data);
   }
 });
