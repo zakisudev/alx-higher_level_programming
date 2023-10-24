0771ee2b8cf2eef10488602541d1753c162e6a5a
zakisu250 <zakisu250@gmail.com>
2023-10-24 10:46:33 +0300

    completed tasks by userId

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, _res, body) {
+  if(err) {
+    console.log(err);
+  } else {
+    const completedTasks = {};
+    body = JSON.parse(body);
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+      if (completed && !completedTasks[userId]) {
+        completedTasks[userId] = 0;
+      }
+      if (completed) ++completedTasks[userId];
+    }
+    console.log(conpletedTasks);
+  }
+});
