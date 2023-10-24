a27eb35931bfa451ed54d8106fe143f885a31693
zakisu250 <zakisu250@gmail.com>
2023-10-24 16:30:51 +0300

    api url corrected

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx.tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
